673c10ffeb3e1f4cc7557e9601e2558e332b23a6
yuhakare <iheuyo927@yahoo.co.jp>
2021-03-23 00:22:32 +0900

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,15 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
-import Example from './components/Example'
 import Tab from './components/Tab'
 import FollowButton from './components/FollowButton'
 
 
 const app = new Vue({
   el: '#app',
-
-
   components: {
     ArticleLike,
     Tab,
-    Example,
     FollowButton,
   }
 })
